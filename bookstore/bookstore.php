e07e9b8b0e5a217ab11f24855278c48ad1c12f01
Jonathan Bossenger <jonathanbossenger@gmail.com>
2024-08-22 14:50:58 +0200

    Committing bookstore-1.0.0

--- bookstore/bookstore.php
+++ bookstore/bookstore.php
@@ -2,7 +2,8 @@
 /**
  * Plugin Name: Bookstore
  * Description: A plugin to manage books
- * Version: 1.0.1
+ * Version: 1.0
+ *
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -26,7 +27,6 @@ function bookstore_register_book_post_type() {
 		'public'       => true,
 		'has_archive'  => true,
 		'show_in_rest' => true,
-		'rest_base'    => 'books',
 		'supports'     => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt' ),
 	);
 
@@ -61,33 +61,10 @@ function bookstore_add_isbn_to_quick_edit( $keys, $post ) {
 	return $keys;
 }
 
-add_action( 'admin_menu', 'bookstore_add_booklist_submenu', 11 );
-function bookstore_add_booklist_submenu() {
-	add_submenu_page(
-		'edit.php?post_type=book',
-		'Book List',
-		'Book List',
-		'edit_posts',
-		'book-list',
-		'bookstore_render_booklist'
-	);
-}
-
-function bookstore_render_booklist() {
-	?>
-	<div class="wrap" id="bookstore-booklist-admin">
-		<h1>Actions</h1>
-		<button id="bookstore-load-books">Load Books</button>
-		<button id="bookstore-fetch-books">Fetch Books</button>
-		<h2>Books</h2>
-		<textarea id="bookstore-booklist" cols="125" rows="15"></textarea>
-	</div>
-	<?php
-}
-
 add_action( 'wp_enqueue_scripts', 'bookstore_enqueue_scripts' );
 function bookstore_enqueue_scripts() {
-	if ( ! is_singular( 'book' ) ) {
+	$post = get_post();
+	if ( 'book' !== $post->post_type ) {
 		return;
 	}
 	wp_enqueue_style(
@@ -95,18 +72,7 @@ function bookstore_enqueue_scripts() {
 		plugins_url() . '/bookstore/bookstore.css'
 	);
 	wp_enqueue_script(
-		'bookstore-script',
-		plugins_url() . '/bookstore/bookstore.js',
-	);
-}
-
-add_action('admin_enqueue_scripts', 'bookstore_admin_enqueue_scripts');
-function bookstore_admin_enqueue_scripts(){
-	wp_enqueue_script(
-		'bookstore-admin-script',
-		plugins_url() . '/bookstore/admin_bookstore.js',
-		array( 'wp-api', 'wp-api-fetch' ),
-		'1.0.0',
-		true
+		'bookstyle-script',
+		plugins_url() . '/bookstore/bookstore.js'
 	);
 }
